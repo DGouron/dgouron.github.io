9a4e86efff149b1ce8f872fcb6a59d040334a2b4
DGouron <damien.gouron@gmail.com>
2022-04-05 14:38:49 +0200

    Refactoring

--- src/tools.js
+++ src/tools.js
@@ -2,12 +2,13 @@
 /  Copyright all right reserved Damien Gouron - 2022
 /  
 */
+const configColorPicker = './config/config_color_picker.json';
 
 function getRandomColor(configFileName = null) {
     if(configFileName == null){
         return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
     }else{
-
+        //TO DO
     }
 }
 
